a6982deffc95c150fb1be418528e132119561010
Jan Omacka <omackaj@gmail.com>
2018-10-12 12:19:58 +0200

    Add countdown screen and fetching

---
+++ src/Main.elm
@@ -0,0 +1,122 @@
+module Main exposing (main)
+
+import Api exposing (deadlineUrl)
+import Browser
+import Debug
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Task
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { deadline : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model ""
+    , send (Fetch 39)
+    )
+
+
+
+-- UPDATE
+
+
+send : msg -> Cmd msg
+send msg =
+    Task.succeed msg
+        |> Task.perform identity
+
+
+type Msg
+    = Fetch Int
+    | Fetched (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Fetch id ->
+            ( model, fetchDeadline model )
+
+        Fetched result ->
+            case result of
+                Ok timeStamp ->
+                    ( { model | deadline = timeStamp }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+
+-- HTTP
+
+
+encodeBody : String -> Encode.Value
+encodeBody value =
+    Encode.object
+        [ ( "query", Encode.string ("query { deadline(id:" ++ value ++ ") { date }}") ) ]
+
+
+fetchDeadline : Model -> Cmd Msg
+fetchDeadline model =
+    Http.send Fetched (Http.post deadlineUrl (encodeBody "56" |> Http.jsonBody) responseDecoder)
+
+
+responseDecoder : Decode.Decoder String
+responseDecoder =
+    Decode.field "data" (Decode.field "deadline" (Decode.field "date" Decode.string))
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ style "background" "#010001"
+        , style "padding-top" "6rem"
+        , style "height" "100vh"
+        , style "weight" "100vw"
+        ]
+        [ titleView
+        , p
+            [ style "color" "rgba(255, 255, 255, 0.8)" ]
+            [ text <| Debug.toString <| model.deadline ]
+
+        -- , p [ style "color" "rgba(255, 255, 255, 0.8)" ] [ text <| Debug.toString <| model ]
+        ]
+
+
+titleView =
+    h1
+        [ style "color" "rgba(255, 255, 255, 0.8)"
+        , style "font-size" "2em"
+        , style "text-align" "center"
+        , style "padding-bottom" "5rem"
+        , style "letter-spacing" "0.2rem"
+        ]
+        [ text "Countdowner" ]
